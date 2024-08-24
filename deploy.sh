573ff6ccce5b1ccee519def4ddffb9f0be2c2abf
TheShadowfish <shadowfish@yandex.ru>
2024-08-24 18:15:07 +0300

    add CD

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ python3 -m venv venv
 source env/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py migrate
-python3 manage.py collectstatic --n0-input
+python3 manage.py collectstatic --no-input
 deactivate
