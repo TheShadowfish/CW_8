d71082b3081fe2e2836a6823ce7727a4d7b045a2
TheShadowfish <shadowfish@yandex.ru>
2024-08-26 10:18:48 +0300

    add CD - ci docker-compose

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
-        # "HOST": os.getenv("POSTGRES_HOST"),
-        # "PORT": os.getenv("POSTGRES_PORT"),
-        # "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
 
     }
 }
