12e4479cbece8e7cf69b98f48eeeb3466d651dd0
TheShadowfish <shadowfish@yandex.ru>
2024-08-26 10:15:07 +0300

    add CD - ci docker-compose

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-python3 -m venv venv
-source env/bin/activate
-pip3 install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
-deactivate
+#python3 -m venv venv
+#source env/bin/activate
+#pip3 install -r requirements.txt
+#python3 manage.py migrate
+#python3 manage.py collectstatic --no-input
+
+docker-compose up --build -d
+dockerdocmpose exec app python manage.py migrate
+#dockerdocmpose exec app python manage.py createsuperuser
